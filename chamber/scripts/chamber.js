32c8b87a03182a1817843122c31f8319338c7c8e
Cherry Machado <cherry5467@gmail.com>
2025-01-22 19:51:51 -0300

    Update chamber.js

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -18,7 +18,7 @@ hamburguerElement.addEventListener("click", () => {
 
 // Directory Website
 
-const url = "../data/members.json";
+const url = "/chamber/data/members.json";
 
 async function getMembers() {
   try {

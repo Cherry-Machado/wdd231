c4358c20c5ef92eb38596d4874f4d394ba68f9da
Cherry-Machado <Cherry5467@gmail.com>
2025-02-07 21:03:22 -0300

    Updating modal

--- scripts/date-ham.js
+++ scripts/date-ham.js
@@ -128,13 +128,17 @@ const courseDetails = document.getElementById("course-details");
 // Function to display course details in the modal
 function displayCourseDetails(course) {
   courseDetails.innerHTML = `
-    <button id="closeModal">❌</button>
-    <h2>${course.subject} ${course.number}</h2>
-    <h3>${course.title}</h3>
-    <p><strong>Credits</strong>: ${course.credits}</p>
-    <p><strong>Certificate</strong>: ${course.certificate}</p>
-    <p>${course.description}</p>
+    <div class="title-modal">
+      <h2>${course.subject} ${course.number}</h2>
+      <button id="closeModal">❌</button>
+    </div>
+    <h3>${course.title}</h3><br>
+    <p><strong>Credits</strong>: ${course.credits}</p><br>
+    <p><strong>Certificate</strong>: ${course.certificate}</p><br>
+    <p><strong>Description</strong>: ${course.description}</p><br>
     <p><strong>Technologies</strong>: ${course.technology.join(", ")}</p>
+    
+    
   `;
   courseDetails.showModal();
 

9d561c511bb7a25a92c30ecf194ac86a79b0dffe
Cherry-Machado <Cherry5467@gmail.com>
2025-02-16 21:26:05 -0300

    Updating

--- finalproject/scripts/nytbookshub.js
+++ finalproject/scripts/nytbookshub.js
@@ -2,6 +2,28 @@ import { getNytBestSellers } from './apinytbookshub.js';
 import { displayBooksCards } from './displaybestbookscards.js';
 import { initializeBuyOptions } from './buybestbooks.js';
 
+// hamburguerElement.addEventListener("click") callback
+const hamburguerElement = document.querySelector("#myButton");
+const navElement = document.querySelector(".menuLinks");
+
+hamburguerElement.addEventListener("click", () => {
+  navElement.classList.toggle("open");
+  hamburguerElement.classList.toggle("open");
+});
+
+const currentOptionHome = document.querySelector(".home-menu");
+const currentOptionAbout = document.querySelector(".about-menu");
+
+currentOptionHome.addEventListener("click", () => {
+    currentOptionAbout.classList.remove("current-menu-item");
+    currentOptionHome.classList.add("current-menu-item");
+});
+
+currentOptionAbout.addEventListener("click", () => {
+    currentOptionAbout.classList.add("current-menu-item");
+    currentOptionHome.classList.remove("current-menu-item");
+});
+
 async function initializeApp() {
     try {
         const data = await getNytBestSellers();
